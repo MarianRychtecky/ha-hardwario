7a8cd3bb7ed5b1d18e22affdb745c01a2a074da4
Marian Rychtecky <marian@mari.cz>
2021-12-29 13:59:49 +0100

    Added debug support

--- run.sh
+++ run.sh
@@ -6,7 +6,15 @@ MQTT_PASSWORD=$(bashio::config 'mqtt_password')
 MQTT_HOST=$(bashio::config 'mqtt_host')
 MQTT_HOST_PORT=$(bashio::config 'mqtt_host_port')
 USB_PORT=$(bashio::config 'usb_port')
+DEBUG=$(bashio::config 'debug')
 
 bashio::log.info "Running bcg on ${USB_PORT}. Connecting to ${MQTT_HOST} on port ${MQTT_HOST_PORT} using username ${MQTT_USERNAME} ..."
 
-bcg -H ${MQTT_HOST} -P ${MQTT_HOST_PORT} --mqtt-username ${MQTT_USERNAME} --mqtt-password ${MQTT_PASSWORD} -d ${USB_PORT} 
+if [ "$DEBUG" = true ]
+ then 
+  bashio::log.info "DEBUG is ON"
+  bcg -H ${MQTT_HOST} -P ${MQTT_HOST_PORT} --mqtt-username ${MQTT_USERNAME} --mqtt-password ${MQTT_PASSWORD} -d ${USB_PORT} --debug
+ else
+  bashio::log.info "DEBUG is OFF"
+  bcg -H ${MQTT_HOST} -P ${MQTT_HOST_PORT} --mqtt-username ${MQTT_USERNAME} --mqtt-password ${MQTT_PASSWORD} -d ${USB_PORT}
+fi
